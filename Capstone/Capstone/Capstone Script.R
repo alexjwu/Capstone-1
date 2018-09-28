2a41ef62e4ab2906373aacde074ebc7d6e827808
alexjwu <40258147+alexjwu@users.noreply.github.com>
2018-09-27 22:59:49 -0700

    Add files via upload

--- Capstone/Capstone/Capstone Script.R
+++ Capstone/Capstone/Capstone Script.R
@@ -1,10 +1,6 @@
 
 # Import Library ----------------------------------------------------------
 
-
-
-
-
 library(readxl)
 winemag_data_130k_v2 <- read_excel("winemag-data-130k-v2.xlsx")
 View(winemag_data_130k_v2)
@@ -14,55 +10,159 @@ Countrykey <- read_excel("Country key.xlsx")
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(corrplot)
 
 
 # Data Exploration ---------------------------------------------------------- 
-DF <- winemag_data_130k_v2
-DF %>% summary(price)
+WineList <- winemag_data_130k_v2
+WineList %>% summary(price)
 
-# Putting missing price as median price ---------------------------------------------------------- 
-DF <- DF %>% mutate(`price` = ifelse(is.na(price), "25", price))
+# Putting missing price as median price ----------------------------------------------------------no double quotes
+WineList <- WineList %>% mutate(price = ifelse(is.na(price), 25, price))
 
-# Remove un-needed data ---------------------------------------------------------- 
+Regct <- count(WineList, vars = region, points)
+
+WineList <- WineList %>% mutate('price' = as.numeric(price))
+
+
+# Data Engineering ---------------------------------------------------------- 
+WineList <- left_join(WineList, Wine_type, by = "variety")
+WineList <- left_join(WineList, Country_Key, by = "country")
+
+WineList <- WineList %>% mutate(WesternEurope = ifelse(region == "Western Europe", 1, ifelse(is.na(Type), 0, 0)))
+WineList <- WineList %>% mutate(NorthAmerica = ifelse(region == "North America", 1, ifelse(is.na(Type), 0, 0)))
+WineList <- WineList %>% mutate(SouthAmerica = ifelse(region == "South America", 1, ifelse(is.na(Type), 0, 0)))
+WineList <- WineList %>% mutate(Africa = ifelse(region == "Africa", 1, ifelse(is.na(Type), 0, 0)))
+WineList <- WineList %>% mutate(India = ifelse(region == "India", 1, ifelse(is.na(Type), 0, 0)))
+WineList <- WineList %>% mutate(CentralAsia = ifelse(region == "Central Asia", 1, ifelse(is.na(Type), 0, 0)))
+WineList <- WineList %>% mutate(MiddleEast = ifelse(region == "Middle East", 1, ifelse(is.na(Type), 0, 0)))
+WineList <- WineList %>% mutate(SouthEEurope = ifelse(region == "South Eastern Europe", 1, ifelse(is.na(Type), 0, 0)))
+WineList <- WineList %>% mutate(EasternEurope = ifelse(region == "Eastern Europe", 1, ifelse(is.na(Type), 0, 0)))
+WineList <- WineList %>% mutate(SouthPacific = ifelse(region == "South Pacfic", 1, ifelse(is.na(Type), 0, 0)))
+WineList <- WineList %>% mutate(CentralAsia = ifelse(region == "Central Asia", 1, ifelse(is.na(Type), 0, 0)))
+WineList <- WineList %>% mutate(EastAsia = ifelse(region == "East Asia", 1, ifelse(is.na(Type), 0, 0)))
+WineList <- WineList %>% mutate(NotA = ifelse(is.na(region) , 1, ifelse(is.na(Type), 0, 0)))
+
+WineList <- WineList %>% mutate(White = ifelse(Type == "White" , 1, ifelse(is.na(Type), 0, 0)))
+WineList <- WineList %>% mutate(Red = ifelse(Type == "Red" , 1, ifelse(is.na(Type),0,0)))
+
+WineList <- WineList %>% mutate("Mpoints" =  (points - mean(points)))
+WineList <- WineList %>% mutate("SDpoints" =  (points - mean(points)) /sd(points))
+
+WineList <- WineList %>% mutate("Mprice" =  (price - mean(price)))
+WineList <- WineList %>% mutate("SDprice" =  (price - mean(price)) /sd(price))
+
+# New Table ---------------------------------------------------------- 
+WLR <- WineList[, c(5, 6, 17:30)]
+
+# Corrplot ---------------------------------------------------------- 
+M <- cor(WLR, use = "complete.obs")
+corrplot(M, method = "number", type = "upper", col = "black")
+
+
+# Plots ---------------------------------------------------------- 
+# Price and Points by region
+ggplot(WineList, aes(x=Mprice, y=Mpoints)) + geom_point(aes(color=region), 
+    size = 1) + geom_smooth(aes(colour=region), method="lm") + 
+    scale_y_continuous(trans = "log1p") + scale_x_continuous(trans="log1p")
+
+ggplot(WineList, aes(x=price, y=SDpoints)) + geom_point(aes(color=region),
+    size = 1) + geom_smooth(aes(colour=region), method="lm") + 
+    scale_y_continuous(trans = "log1p") + scale_x_continuous(trans="log1p")
+
+ggplot(WineList, aes(x=SDpoints)) + geom_density(aes(fill=region),
+                                                 alpha = 0.4)
+
+ggplot(WineList, aes(x=Mprice, y=Mpoints)) + geom_point(aes(color=Type), 
+    size = 1) + geom_smooth(aes(colour=Type), method="lm") + 
+    scale_y_continuous(trans = "log1p") + scale_x_continuous(trans="log1p")
+
+
+# Do not use:
+
+# Data Exploration ---------------------------------------------------------- 
+reg.pt.reg <- subset(WineList, select = c("points", "region"))
+summary(reg.pt.reg)
+cor(reg.pt.reg)
+
+reg.pr.reg <- subset(WineList, select = c("price", "region"))
+summary(reg.pr.reg)
+cor(reg.pr.reg)
+
+reg.pr.pt <- subset(WineList, select = c("price", "points"))
+summary(reg.pr.pt)
+cor(reg.pr.pt)
+
+# Correlation ----------------------------------------------------------
+sat.mod <- lm(points ~ price + region + Type + variety, data = WineList)
+summary(sat.mod)
+coef(summary(sat.mod))
+anova(sat.mod)
+
+sat.mod2 <- lm(points ~ price, data = WineList)
+summary(sat.mod2)
+
+sat.mod3 <- lm(points ~ region, data = WineList)
+summary(sat.mod3)
+
+sat.mod4 <- lm(points ~ region + price, data = WineList)
+summary(sat.mod4)
+
+# Remove un-needed data ---------------------------------------------------------- Use column names not indices
 DF3 <- DF[, c(1,2,5,6,7,13,14)]
 
 WineList <- DF3
 
-rm(DF, DF2, DF3, winemag_data_130k_v2)
+rm(df, Exm, Grpby, reg.pr.pt, reg.pr.reg, reg.pt.reg, Regct, sat.mod, sat.mod2, sat.mod3, sat.mod4, WLbyR, WLNA, WLV)
 
-WineList <- WineList %>% mutate('price' = as.numeric(price))
-
-WineList <- WineList %>% mutate(`price_cat` = 
-                                  ifelse(price >= 6 & price <= 10, "10",
-                                  ifelse(price >= 11 & price <= 15, "15", 
-                                  ifelse(price >= 16 & price <= 20, "20", 
-                                  ifelse(price >= 21 & price <= 25, "25", 
-                                  ifelse(price >= 26 & price <= 30, "30", 
-                                  ifelse(price >= 1 & price <= 5, "5", 
-                                  ifelse(price >= 31 & price <= 35, "35", 
-                                  ifelse(price >= 36 & price <= 40, "40", 
-                                  ifelse(price >= 41 & price <= 45, "45", 
-                                  ifelse(price >= 46 & price <= 50, "50", 
-                                  ifelse(price >= 51 & price <= 55, "55", 
-                                  ifelse(price >= 56 & price <= 60, "60", 
-                                  ifelse(price >= 61 & price <= 65, "65", 
-                                  ifelse(price >= 66 & price <= 70, "70", 
-                                  ifelse(price >= 71 & price <= 75, "75", 
-                                  ifelse(price >= 76 & price <= 80, "80", 
-                                  ifelse(price >= 81 & price <= 85, "85", 
-                                  ifelse(price >= 86 & price <= 90, "90", 
-                                  ifelse(price >= 91 & price <= 95, "95", 
-                                  ifelse (price >= 96 & price <= 100, "100", "no")))))))))))))))))))))
-
-# Data Engineering ---------------------------------------------------------- 
-WineList <- left_join(WineList, Wine_type, by = "variety")
-
-WineList <- left_join(WineList, Country_Key, by = "country")
+WineList <- WineList %>% mutate("10" = ifelse(price > 0 & price <= 10, 1, 0))
+WineList <- WineList %>% mutate("20" = ifelse(price > 10 & price <= 20, 1, 0))
+WineList <- WineList %>% mutate("30" = ifelse(price > 20 & price <= 30, 1, 0))
+WineList <- WineList %>% mutate("40" = ifelse(price > 30 & price <= 40, 1, 0))
+WineList <- WineList %>% mutate("50" = ifelse(price > 40 & price <= 50, 1, 0))
+WineList <- WineList %>% mutate("60" = ifelse(price > 50 & price <= 60, 1, 0))
+WineList <- WineList %>% mutate("70" = ifelse(price > 60 & price <= 70, 1, 0))
+WineList <- WineList %>% mutate("80" = ifelse(price > 70 & price <= 80, 1, 0))
+WineList <- WineList %>% mutate("90" = ifelse(price > 80 & price <= 90, 1, 0))
+WineList <- WineList %>% mutate("100" = ifelse(price > 90 & price <= 100, 1, 0))
+WineList <- WineList %>% mutate("100+" = ifelse(price > 100, 1, 0))
 
 
-# Plots ---------------------------------------------------------- 
+WineList <- WineList %>% mutate("price_cat" = ifelse(price > 0 & price <= 10, 1, 
+                                                     ifelse(price > 10 & price <= 20, 10, ifelse(price > 20 & price <= 30, 20, 
+                                                                                                 ifelse(price > 30 & price <= 40, 30, ifelse(price > 40 & price <= 50, 40, 
+                                                                                                                                             ifelse(price > 50 & price <= 60, 50, ifelse(price > 60 & price <= 70, 60, 
+                                                                                                                                                                                         ifelse(price > 70 & price <= 80, 70, ifelse(price > 80 & price <= 90, 80,
+                                                                                                                                                                                                                                     ifelse(price > 90 & price <= 100, 90, ifelse(price > 100, 1000, 0))))))))))))
+
+
+Grpby <- WineList %>% group_by(price_cat, WesternEurope, NorthAmerica, 
+                               SouthAmerica, Africa, India, CentralAsia, NotA, MiddleEast, SouthEEurope, 
+                               EasternEurope, SouthPacific, EastAsia, Red, White)
+
+WLbyR<- Grpby %>% summarise(p = mean(points))
+
+center_x <- function(x) { x - mean(x)}
+z_score <- function(x) { (x - mean(x)) / sd(x)}
+
+M <- cor(WLR, use = "complete.obs")
+
+# Julian example
+ggplot(WineList, aes(x=SDpoints, y=SDprice)) + geom_point(aes(colour=region), size=2) + geom_smooth(aes(colour=region), se=FALSE, method="lm") + facet_grid(region~.) + scale_y_continuous(trans="log1p")
+
+ggplot(WineList, aes(x=SDpoints)) + geom_density(aes(fill=region),
+                                                 alpha = 0.4)
+
+
+
+
+
 ggplot(WineList, aes(points)) + geom_histogram()
+
 ggplot(WineList, aes(price_cut)) + geom_histogram()
-ggplot(WineList, aes(x=log(price))) + geom_histogram(aes(fill=country), alpha=0.40)
-ggplot(WineList, aes(x = price, y = points, fill = Type)) + geom_point() + geom_smooth() +  scale_x_continuous(limits = c(5, 100)) + scale_y_continuous(limits = c(80, 100))
-ggplot(WineList, aes(x=points)) + geom_density(aes(fill=region), alpha=0.40)
+
+ggplot(WineList, aes(x=log(price))) + geom_histogram(aes(fill=region), alpha=0.40)
+
+ggplot(WineList, aes(x = price, y = points, color = Type)) + geom_jitter() + geom_smooth() + scale_x_continuous(limits = c(5, 100)) + scale_y_continuous(limits = c(80, 100))
+
+ggplot(WineList, aes(x=points)) + geom_density(aes(fill=region), alpha=0.50)
